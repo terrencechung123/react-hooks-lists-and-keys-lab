e8a66fe156aaeef9f4037d381db100af217f77f6
Terrence Chung <terrencechung35@gmail.com>
2023-02-02 11:08:50 -0800

    labs

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,20 @@
 import React from "react";
 
 function NavBar() {
-  const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const links = [
+    "home",
+    "about",
+    "projects"
+  ];
+  const listLinks = links.map((link)=>
+  <a key={link} href={"#"+link} >
+    {link}
+    </a>
+  );
+  return (
+    <nav>
+    {listLinks}
+    </nav>)
 }
 
 export default NavBar;
